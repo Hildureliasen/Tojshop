7f9d80a28d73eef311fbd2c512336d17f48850f6
Hildureliasen <hildur.eliasen@gmail.com>
2023-08-28 12:20:08 +0200

    egvetiik

---
+++ hildur.js
@@ -0,0 +1,16 @@
+{
+  powers: ["strength", "dancing "];
+  realName: "Olaf";
+  alias: "Frosty";
+  origin: "home";
+  weaknesses: ["spiders", "mornings"];
+  active: true;
+  isEvil: true;
+  height: 40;
+  birthday: {
+    day: 4;
+    month: 5;
+    year: 3005;
+  }
+  Image: "https://www.pexels.com/photo/snowman-holding-shovel-3334473/";
+}
